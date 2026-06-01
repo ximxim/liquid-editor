a24f34685ecbd8fbd9ffc74cea9aab6245efa096
Azim Ahmed <azim.ahmed7@gmail.com>
2026-05-31 20:22:05 -0400

    fix: use insertTypesEntry instead of rollupTypes to generate dist/index.d.ts

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ['src'] }),
+    dts({ include: ['src'], insertTypesEntry: true }),
   ],
   build: {
     lib: {
